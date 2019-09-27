c8be6287a76ee95db9ca86b668edc6723bd5e579
alek.frohlich <alek.frohlich@gmail.com>
2019-09-26 23:19:02 -0300

    working test1

--- src/kakuro.cpp
+++ src/kakuro.cpp
@@ -25,86 +25,17 @@
 
 
 #include <iostream>
+#ifndef __KAKURO__
+#define __KAKURO__
+    #include "kakuro.hpp"
+#endif  // namespace kakuro.
+#include "test.hpp"
 
-#define BLACK 0             // Black position.
-#define WHITE 1             // White position.
-
-#define LIM 10              // Last permited number.
-
-#define WIDTH 6             // Board width.
-#define HEIGHT 6            // Board Height.
-
-#define LAST_I 5            // Last available line index.
-#define LAST_J 5            // Last available collumn index.
-
-#define FORWARD true        // Going forward in board.
-#define BACKWARD false      // Going backward in board.
-
-#define SOLVED 0            // Solvable board.
-#define UNSOLVABLE 1        // Unsolvable board.
-
-// declarations
+// declarations.
 inline static bool check_line(int,int);
 inline static bool check_col(int,int);
 inline static bool line_repeats(int,int,int);
 inline static bool col_repeats(int,int,int);
-
-/*
- * Black/White position. 
- */
-struct pos {
-    int c;
-    int first;
-    int second;
-};
-
-/*
- * Kakuro board.
- */
-static pos board[HEIGHT][WIDTH];
-
-/*
- * Initialize board.
- */
-static void init_board()
-{
-    board[0][0] = {BLACK, 0, 0};
-    board[0][1] = {BLACK, 12, 0};    
-    board[0][2] = {BLACK, 21, 0};
-    board[0][3] = {BLACK, 0, 0};
-    board[0][4] = {BLACK, 16, 0};
-    board[0][5] = {BLACK, 13, 0};
-    board[1][0] = {BLACK, 0, 17};
-    board[1][1] = {WHITE, 0, 0};    
-    board[1][2] = {WHITE, 0, 0};
-    board[1][3] = {BLACK, 22, 11};
-    board[1][4] = {WHITE, 0, 0};
-    board[1][5] = {WHITE, 0, 0};
-    board[2][0] = {BLACK, 0, 15};
-    board[2][1] = {WHITE, 0, 0};    
-    board[2][2] = {WHITE, 0, 0};
-    board[2][3] = {WHITE, 0, 0};
-    board[2][4] = {WHITE, 0, 0};
-    board[2][5] = {WHITE, 0, 0};
-    board[3][0] = {BLACK, 0, 0};
-    board[3][1] = {BLACK, 4, 13};    
-    board[3][2] = {WHITE, 0, 0};
-    board[3][3] = {WHITE, 0, 0};
-    board[3][4] = {WHITE, 0, 0};
-    board[3][5] = {BLACK, 10, 0};
-    board[4][0] = {BLACK, 0, 18};
-    board[4][1] = {WHITE, 0, 0};    
-    board[4][2] = {WHITE, 0, 0};
-    board[4][3] = {WHITE, 0, 0};
-    board[4][4] = {WHITE, 0, 0};
-    board[4][5] = {WHITE, 0, 0};
-    board[5][0] = {BLACK, 0, 10};
-    board[5][1] = {WHITE, 0, 0};    
-    board[5][2] = {WHITE, 0, 0};
-    board[5][3] = {BLACK, 0, 14};
-    board[5][4] = {WHITE, 0, 0};
-    board[5][5] = {WHITE, 0, 0};
-}
 
 /*
  * Solves a given kakuro board. 
@@ -117,10 +48,15 @@ int solve(int i, int j, int value, bool forward)
     if (board[i][j].c == BLACK && forward)
     {
         /*
+         * Last position is black. 
+         */
+        if (i == LAST_I && j == LAST_J)
+            return SOLVED;
+
+        /*
          * Move forward.
          */
         return solve(((j == LAST_J)? i+1: i), (j+1) % WIDTH, 1, FORWARD);
-
     } 
     
     /*
@@ -131,13 +67,13 @@ int solve(int i, int j, int value, bool forward)
         /*
          * All possible combinations failed.
          */
-        if (j==0 && i==0)
+        if (j == 0 && i == 0)
             return UNSOLVABLE;
 
         /*
          * Backtrack (up).
          */
-        else if (j==0)
+        else if (j == 0)
             return solve(i-1, LAST_J, board[i-1][LAST_J].first+1, BACKWARD);
 
         /*
@@ -153,7 +89,7 @@ int solve(int i, int j, int value, bool forward)
     board[i][j].first = value;
 
     /*
-     * Right is black.
+     * Right is black or last element of row.
      */
     if ((j == LAST_J) || ((j != LAST_J) && board[i][j+1].c == BLACK))
     {
@@ -172,28 +108,16 @@ int solve(int i, int j, int value, bool forward)
     }
 
     /*
-     * Down is black.
+     * Down is black or last element of collumn.
      */
     if ((i == LAST_I) || ((i != LAST_I) && board[i+1][j].c == BLACK))
     {    
         /*
-         * Tried all possible
-         * numbers for this position.
+         * Tried all possible numbers for this
+         * position, backtrack (left).
          */
         if (value == LIM)
-        {
-            /*
-             * Backtrack (up). 
-             */
-            if (j == 0)
-                return solve(i-1, LAST_J, board[i-1][LAST_J].first+1, BACKWARD);
-
-            /*
-             * Backtrack (left). 
-             */
-            else
-                return solve(i, j-1, board[i][j-1].first+1, BACKWARD);   
-        }
+            return solve(i, j-1, board[i][j-1].first+1, BACKWARD);   
 
         /*
          * Try next possible value. 
@@ -217,7 +141,18 @@ int solve(int i, int j, int value, bool forward)
     /*
      * Right and down are white.
      */
-    return solve(((j == LAST_J)? i+1: i), (j+1) % WIDTH, 1, FORWARD);
+    if (line_repeats(i, j, value) || col_repeats(i, j, value))
+        /*
+         * White middle-man repeats
+         * some value, try again
+         * with next possible value. 
+         */
+        return solve(i, j, value+1, FORWARD);
+    else
+        /*
+         * Move forward. 
+         */
+        return solve(((j == LAST_J)? i+1: i), (j+1) % WIDTH, 1, FORWARD);
 }
 
 /*
@@ -225,7 +160,7 @@ int solve(int i, int j, int value, bool forward)
  */
 static bool inline check_line(int line, int j)
 {
-    static int sum = 0;
+    int sum = 0;
     while (board[line][j].c == WHITE)
     {
         sum += board[line][j].first;
@@ -239,7 +174,7 @@ static bool inline check_line(int line, int j)
  */
 static bool inline check_col(int i, int col)
 {
-    static int sum = 0;
+    int sum = 0;
     while (board[i][col].c == WHITE)
     {
         sum += board[i][col].first;
@@ -253,12 +188,19 @@ static bool inline check_col(int i, int col)
  */
 static bool inline line_repeats(int line, int j, int value)
 {
-    while (board[line][j].c == WHITE)
-    {
-        if (board[line][--j].first == value)
+    while (true)
+    {   
+        --j;
+
+        /*
+         * Stop after hitting
+         * a black position. 
+         */
+        if(board[line][j].c == BLACK)
+            return false;
+        if (board[line][j].first == value)
             return true;
     }
-    return false;
 }
 
 /*
@@ -266,12 +208,19 @@ static bool inline line_repeats(int line, int j, int value)
  */
 static bool inline col_repeats(int i, int col, int value)
 {
-    while (board[i][col].c == WHITE)
+    while (true)
     {
-        if (board[--i][col].first == value)
+        --i;
+
+        /*
+         * Stop after hitting
+         * a black position. 
+         */
+        if(board[i][col].c == BLACK)
+            return false;
+        if (board[i][col].first == value)
             return true;
     }
-    return false;
 }
 
 /*
